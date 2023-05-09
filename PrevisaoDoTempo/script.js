14a2562158be0e7fcbb5ab0f782e9cf089908816
longueneck <kevinlongue@hotmail.com>
2023-05-09 02:33:40 -0300

    [FEAT]
    New Feat

--- PrevisaoDoTempo/script.js
+++ PrevisaoDoTempo/script.js
@@ -1,9 +1,29 @@
-
-
 const key = "e415d74004de433c55f2fe1a9a7fa9e8";
 
-function cliqueiNoBotao() {
-    const input = document.querySelector(".input-cidade").value
+async function buscarCidade(cidade) {
+  const dados = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`
+  ).then((resposta) => resposta.json());
 
-    console.log(input)
+  console.log(dados);
 }
+
+function cliqueiNoBotao() {
+  const cidade = document.querySelector(".input-cidade").value;
+  buscarCidade(cidade);
+}
+
+
+// const key = "e415d74004de433c55f2fe1a9a7fa9e8";
+
+// async function buscarCidade(cidade){
+
+//     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then( resposta => resposta.json() )
+
+//     console.log(dados)
+// function cliqueiNoBotao() {
+//     const cidade = document.querySelector(".input-cidade").value
+
+//     buscarCidade(cidade)
+// }
+// }
