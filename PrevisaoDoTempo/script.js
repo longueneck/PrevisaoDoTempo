8ec8e8fa09336cd1ce78cc146ef430cdad3e8d8f
longueneck <kevinlongue@hotmail.com>
2023-05-09 03:09:21 -0300

    [FEAT]
    FINISHINg

--- PrevisaoDoTempo/script.js
+++ PrevisaoDoTempo/script.js
@@ -1,11 +1,23 @@
 const key = "e415d74004de433c55f2fe1a9a7fa9e8";
 
+
+function colocarDadosNaTela(dados){
+document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+document.querySelector(".temp").innerHTML = "Temperatura atual é de: " + Math.floor(dados.main.temp) + "ºC"
+document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+}
+
+
+
 async function buscarCidade(cidade) {
   const dados = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`
   ).then((resposta) => resposta.json());
 
-  console.log(dados);
+  colocarDadosNaTela(dados)
 }
 
 function cliqueiNoBotao() {
@@ -13,17 +25,3 @@ function cliqueiNoBotao() {
   buscarCidade(cidade);
 }
 
-
-// const key = "e415d74004de433c55f2fe1a9a7fa9e8";
-
-// async function buscarCidade(cidade){
-
-//     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then( resposta => resposta.json() )
-
-//     console.log(dados)
-// function cliqueiNoBotao() {
-//     const cidade = document.querySelector(".input-cidade").value
-
-//     buscarCidade(cidade)
-// }
-// }
